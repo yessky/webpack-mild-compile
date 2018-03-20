0b277f2532b86ae19b870bbcf967772d65ab77b4
yessky <admin@veryos.com>
2018-03-20 10:25:30 +0800

    fix compatibility for webpack v4

--- index.js
+++ index.js
@@ -5,26 +5,34 @@
     a workaround to prevent webpack endlessly compile
 */
 
+const pluginName = 'MildCompile';
+
 function timefix(compiler) {
   const timefix = 11000;
-  const onWatchRun = function (watching, callback) {
+  let watching = {};
+  const onWatchRun = function () {
     watching.startTime += timefix;
-    callback();
   };
   const onDone = function (stats) {
     stats.startTime -= timefix;
   };
+  const aspectWatch = compiler.watch;
+  compiler.watch = function () {
+    watching = aspectWatch.apply(compiler, arguments);
+    return watching;
+  };
   if (compiler.hooks) {
-    const plugin = { name: 'MildCompilePlugin' };
-    compiler.hooks.watchRun.tapAsync(plugin, onWatchRun);
-    compiler.hooks.done.tapAsync(plugin, onDone);
+    compiler.hooks.watchRun.tap(pluginName, onWatchRun);
+    compiler.hooks.done.tap(pluginName, onDone);
   } else {
     compiler.plugin('watch-run', onWatchRun);
     compiler.plugin('done', onDone);
   }
 }
 
-timefix.Plugin = function () { };
+timefix.Plugin = function () {
+  this.name = pluginName;
+};
 timefix.Plugin.prototype.apply = timefix;
 
 module.exports = timefix;
